07e5303ad9c2cac9310914543314309f7d7ed71e
Kevin Müller <mkevin2006@gmail.com>
2023-05-07 14:47:19 +0200

    Test basic command functionality

--- thermal-camera/src/main.rs
+++ thermal-camera/src/main.rs
@@ -1,3 +1,9 @@
+use std::{process::Command, os::unix::process::CommandExt};
+
 fn main() {
     println!("Hello, world!");
+
+    let mut command = Command::new("echo");
+    command.arg("Hi");
+    command.exec();
 }
