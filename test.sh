f035a6a8cde95d3ec66a46943b66d44b8df807de
Kevin Müller <mkevin2006@gmail.com>
2023-05-09 20:02:58 +0200

    Improve test script

--- test.sh
+++ test.sh
@@ -4,10 +4,15 @@
 
 scp thermal-camera/target/armv7-unknown-linux-musleabihf/debug/thermal-camera thermal-camera@raspberrypi:~/thermal-camera/thermal-camera
 
-ssh thermal-camera@raspberrypi "cd thermal-camera && ./thermal-camera"
+ssh thermal-camera@raspberrypi << EOF
+    cd thermal-camera
+    ./thermal-camera
+EOF
 
 mkdir -p test-output
 
+# Remove the file if it's already there,
+# That way it will be more obvious if an error occurs
 rm -f test-output/test.pgm
 
 scp thermal-camera@raspberrypi:~/thermal-camera/test.pgm test-output
