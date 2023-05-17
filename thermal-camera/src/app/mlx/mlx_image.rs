98b5ec84dee745575d526f14d9f808f2b1bb080f
Kevin Müller <mkevin2006@gmail.com>
2023-05-17 20:30:07 +0200

    Add new function for general grayscale images

--- thermal-camera/src/app/mlx/mlx_image.rs
+++ thermal-camera/src/app/mlx/mlx_image.rs
@@ -8,3 +8,14 @@ pub fn grayscale_test(temperatures: &[f32]) -> [u8; PIXEL_COUNT] {
     }
     return res;
 }
+
+pub fn grayscale(temperatures: [f32; PIXEL_COUNT], temp_min: f32, temp_max: f32) -> [u8; PIXEL_COUNT] {
+    let mut res: [u8; PIXEL_COUNT] = [0x00; PIXEL_COUNT];
+
+    for i in 0..PIXEL_COUNT {
+        let value: f32 = (temperatures[i] - temp_min) * (255.0 / temp_max);
+        res[i] = value.round().max(0.0).min(255.0) as u8;
+    }
+
+    return res;
+}
