5de6099d8b3a24231cf59c439c400ddd6dc7d64c
Kevin Müller <mkevin2006@gmail.com>
2023-05-17 21:35:07 +0200

    Add more convinience to manual testing

--- upload_bin.sh
+++ upload_bin.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+ORANGE='\033[0;33m'
+NOCOL='\033[0m'
+
 cd thermal-camera
 cargo build
 
 scp target/armv7-unknown-linux-musleabihf/debug/thermal-camera thermal-camera@raspberrypi:~/thermal-camera/thermal-camera
+
+echo -e "${ORANGE}You will now be ssh-redirected onto the Thermal Camera."
+echo -e "Remember to exit when finished.${NOCOL}"
+ssh -Y -t thermal-camera@raspberrypi 'cd thermal-camera; /bin/bash'
