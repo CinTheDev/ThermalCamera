0c18879514a70fe9f8c399fb48596d0c5a0c6a7a
Kevin Müller <mkevin2006@gmail.com>
2023-05-18 15:22:21 +0200

    Update test.sh for new syntax

--- test.sh
+++ test.sh
@@ -8,15 +8,14 @@
 
 ssh thermal-camera@raspberrypi << EOF
     cd thermal-camera
-    ./thermal-camera test.pgm
+    ./thermal-camera test.png
 EOF
-
-mkdir -p test-output
 
 # Remove the file if it's already there,
 # That way it will be more obvious if an error occurs
-rm -f test-output/test.pgm
+rm -rf test-output
+mkdir test-output
 
-scp thermal-camera@raspberrypi:~/thermal-camera/test.pgm test-output
+scp thermal-camera@raspberrypi:~/thermal-camera/test.png test-output
 
 echo "Output image has been copied to test-output/"
