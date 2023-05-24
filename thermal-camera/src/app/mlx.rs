347bddae9f53eef70db2079bde8cbea80cb4c3fc
Kevin Müller <mkevin2006@gmail.com>
2023-05-24 16:36:33 +0200

    Add mlx function for new rgb colored image

--- thermal-camera/src/app/mlx.rs
+++ thermal-camera/src/app/mlx.rs
@@ -14,6 +14,11 @@ pub fn grayscale(temp_min: f32, temp_max: f32) -> [u8; PIXEL_COUNT] {
     return mlx_image::grayscale(temperature_grid, temp_min, temp_max);
 }
 
+pub fn colored_cheap(temp_min: f32, temp_max: f32) -> [u8; PIXEL_COUNT * 3] {
+    let temperature_grid = read_temperatures();
+    return mlx_image::rgb(temperature_grid, temp_min, temp_max);
+}
+
 fn read_temperatures() -> [f32; PIXEL_COUNT] {
     let image_raw = read_raw_image();
     return bsp_mlx::evaluate_image(image_raw);
