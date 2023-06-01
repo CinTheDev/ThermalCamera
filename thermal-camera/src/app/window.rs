9305b833a99bef4c482d9c64e794d1d81f5805a4
Kevin Müller <mkevin2006@gmail.com>
2023-06-01 16:51:56 +0200

    Add basis for eframe window

--- thermal-camera/src/app/window.rs
+++ thermal-camera/src/app/window.rs
@@ -2,5 +2,28 @@ use eframe::egui;
 pub use super::Opt;
 
 pub fn open_window(args: Opt) {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Thermal Camera",
+        native_options,
+        Box::new(|cc| Box::new(ThermalApp::new(cc))),
+    )
+    .unwrap();
+}
 
+#[derive(Default)]
+struct ThermalApp {
+
+}
+
+impl ThermalApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        return Self::default();
+    }
+}
+
+impl eframe::App for ThermalApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
+    }
 }
