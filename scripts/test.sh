f32d595a969a428d3322f9bfb24bc271c4c2ce4b
Kevin Müller <mkevin2006@gmail.com>
2023-06-01 17:09:16 +0200

    Remove unessecary AND operators

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ( 
-    cd thermal-camera &&
-    cargo build &&
+    cd thermal-camera
+    cargo build
     scp target/armv7-unknown-linux-musleabihf/debug/thermal-camera thermal-camera@raspberrypi:~/thermal-camera/thermal-camera
 )
 
