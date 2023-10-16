4f4026179ed786f67973bc7941ea89ce7e06684f
Kevin Müller <mkevin2006@gmail.com>
2023-10-16 17:12:57 +0200

    Create image module and move related stuff there

---
+++ thermal-camera/src/app/window/image.rs
@@ -0,0 +1,97 @@
+use super::{egui, ThermalApp, mlx};
+
+impl ThermalApp {
+    pub fn show_image(&mut self, ui: &mut egui::Ui) {
+        let texture: &egui::TextureHandle = self.picture.get_or_insert_with(|| {
+            let raw_img = self.raw_picture.get_or_insert_with(|| {
+                [0x00; mlx::PIXEL_COUNT * 3]
+            });
+
+            let img = egui::ColorImage::from_rgb(
+                [mlx::PIXELS_WIDTH, mlx::PIXELS_HEIGHT],
+                raw_img
+            );
+
+            ui.ctx()
+                .load_texture("Picture", img, self.picture_options)
+        });
+
+        let height = ui.available_height();
+        let width = height * (mlx::PIXELS_WIDTH as f32 / mlx::PIXELS_HEIGHT as f32);
+        let size = egui::Vec2 {x: width, y: height};
+
+        let response = ui.image(texture, size);
+        self.check_clicked(ui, response);
+    }
+    
+    pub fn update_image(&mut self, ctx: &egui::Context) {
+        // Don't update image if not supposed to
+        if !self.rx_active { return }
+
+        let rx = self.get_thread_receiver(ctx);
+
+        let rx_img = rx.try_recv();
+
+        if rx_img.is_ok() {
+            let raw_img = rx_img.unwrap();
+            self.raw_picture.replace(raw_img.pixels);
+            self.temperature_grid.replace(raw_img.temperature_grid);
+
+            let img = egui::ColorImage::from_rgb(
+                [mlx::PIXELS_WIDTH, mlx::PIXELS_HEIGHT],
+                &raw_img.pixels
+            );
+
+            self.picture.as_mut().unwrap().set(img, self.picture_options);
+
+            self.scale_bound = (raw_img.min_temp, raw_img.max_temp);
+        }
+    }
+
+    pub fn check_clicked(&mut self, ui: &mut egui::Ui, response: egui::Response) {
+        // TODO: Only when dragging the mouse this is registered
+        // Could be the display, but should be investigated
+        let mut left_mouse_down = false;
+        ui.input(|i| left_mouse_down = i.pointer.button_down(egui::PointerButton::Primary));
+        if ! left_mouse_down {
+            return;
+        }
+
+        let pos_option = response.hover_pos();
+        if pos_option.is_none() {
+            return;
+        }
+
+        // Calculate position inside image
+        let pos = pos_option.unwrap();
+
+        let rect_image = response.rect;
+
+        let uv = egui::Pos2::new(
+            (pos.x - rect_image.left()) / rect_image.right(),
+            (pos.y - rect_image.top()) / rect_image.bottom()
+        );
+
+        let img_coord: (usize, usize) = (
+            (uv.x * mlx::PIXELS_WIDTH as f32).floor().min(mlx::PIXELS_WIDTH as f32).max(0.0) as usize,
+            (uv.y * mlx::PIXELS_HEIGHT as f32).floor().min(mlx::PIXELS_HEIGHT as f32).max(0.0) as usize,
+        );
+
+        let index = img_coord.0 + img_coord.1 * mlx::PIXELS_WIDTH;
+        let temperature = self.temperature_grid.unwrap_or([0.0; mlx::PIXEL_COUNT])[index];
+        let temp_string = format!("{:.1} °C", temperature);
+
+        let bg_col = egui::Color32::BLACK;
+        let txt_col = egui::Color32::WHITE;
+        
+        // Draw
+        let painter = ui.painter();
+        let txt_galley = painter.layout_no_wrap(temp_string, egui::FontId::default(), txt_col);
+        let bg_rect = txt_galley.rect
+            .translate(pos.to_vec2())
+            .expand(5.0);
+
+        painter.rect_filled(bg_rect, egui::Rounding::none(), bg_col);
+        painter.galley(pos, txt_galley);
+    }
+}
