b895d598e3d08f399c875b2c93deea1fa895f07f
Kevin Müller <mkevin2006@gmail.com>
2023-10-16 18:08:08 +0200

    Attempt to place buttons with allocate_space() [failed]

--- thermal-camera/src/app/window/controls.rs
+++ thermal-camera/src/app/window/controls.rs
@@ -1,7 +1,12 @@
 use super::{egui, ThermalApp};
 
 pub fn show(app: &mut ThermalApp, ui: &mut egui::Ui) {
+    let min_size = egui::Vec2::new(100.0, 200.0); // TODO: Adjust this
+    let allocated_space = ui.allocate_space(min_size);
+
     ui.vertical(|ui| {
+        ui.expand_to_include_rect(allocated_space.1);
+
         let button_freeze = ui.button("Freeze image");
         let button_save = ui.add_enabled(
             app.usb_detected,
