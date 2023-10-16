fa266375f65eda7dbd9430e94b93ebf28b9615f2
Kevin Müller <mkevin2006@gmail.com>
2023-10-16 17:44:34 +0200

    Move buttons to new layout in controls.rs

--- thermal-camera/src/app/window/controls.rs
+++ thermal-camera/src/app/window/controls.rs
@@ -1,14 +1,27 @@
-use super::{egui, ThermalApp, mlx};
+use super::{egui, ThermalApp};
 
 pub fn show(app: &mut ThermalApp, ui: &mut egui::Ui) {
+    ui.vertical(|ui| {
+        let button_freeze = ui.button("Freeze image");
+        let button_save = ui.add_enabled(
+            app.usb_detected,
+            egui::Button::new("Save image")
+        );
 
+        if button_freeze.clicked() {
+            on_button_freeze(app);
+        }
+        if button_save.clicked() {
+            on_button_save(app);
+        }
+    });
 }
 
-fn button_freeze(app: &mut ThermalApp) {
+fn on_button_freeze(app: &mut ThermalApp) {
     app.rx_active = !app.rx_active;
 }
 
-fn button_save(app: &mut ThermalApp) {
+fn on_button_save(app: &mut ThermalApp) {
     app.save_image();
     println!("Image saved");
 }
