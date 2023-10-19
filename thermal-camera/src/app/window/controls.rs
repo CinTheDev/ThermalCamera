c2dded45afbb5c705b360466c90531374db85d37
Kevin Müller <mkevin2006@gmail.com>
2023-10-19 16:20:09 +0200

    Manually set size of buttons

--- thermal-camera/src/app/window/controls.rs
+++ thermal-camera/src/app/window/controls.rs
@@ -3,14 +3,18 @@ use super::CONTROLS_X_SPACE;
 
 pub fn show(app: &mut ThermalApp, ui: &mut egui::Ui) {
     let width_allocate = CONTROLS_X_SPACE * app.window_size.x;
+    let height_buttons = ui.available_height() / 2.0;
+    let size_buttons = egui::Vec2::new(width_allocate, height_buttons);
 
     ui.vertical_centered(|ui| {
-        ui.set_min_width(width_allocate);
+        //ui.set_min_width(width_allocate);
 
-        let button_freeze = ui.button("Freeze image");
+        let button_freeze = ui.add(
+            egui::Button::new("Freeze image").min_size(size_buttons)
+        );
         let button_save = ui.add_enabled(
             app.usb_detected,
-            egui::Button::new("Save image")
+            egui::Button::new("Save image").min_size(size_buttons)
         );
 
         if button_freeze.clicked() {
