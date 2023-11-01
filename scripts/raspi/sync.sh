38ea8fe06fc849adca8eb789138234ec784ce41e
Kevin Müller <mkevin2006@gmail.com>
2023-11-01 15:25:56 +0100

    Finish up sync.sh

--- scripts/raspi/sync.sh
+++ scripts/raspi/sync.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Copy script
+# Copy this directory onto raspi and connect via ssh
 
-#rsync -r ./scripts/raspi/create_user.sh temp@thermal-camera:~/thermal-camera/
-#rsync ./scripts/raspi/config.sh temp@thermal-camera:~/thermal-camera/
-#ssh temp@thermal-camera 'cd thermal-camera && ./create_user.sh'
-
-rsync -r --delete ./scripts/raspi pi@thermal-camera:~/thermal-camera-config/
-ssh -t pi@thermal-camera 'cd thermal-camera-config ;/bin/bash'
+rsync -r --delete ./scripts/raspi/ pi@thermal-camera:~/thermal-camera-config/
+ssh -t pi@thermal-camera 'cd thermal-camera-config; /bin/bash'
