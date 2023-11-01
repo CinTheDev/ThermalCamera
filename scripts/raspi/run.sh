2bcc632df17e0ef4270c5afceab6b2053f24c0ee
Kevin Müller <mkevin2006@gmail.com>
2023-11-01 15:11:05 +0100

    Restructure scripts

---
+++ scripts/raspi/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run all scripts inside scripts/ in order
+
+./scripts/create_user.sh
--- scripts/raspi/sync.sh
+++ scripts/raspi/sync.sh
@@ -4,4 +4,4 @@
 
 rsync -r ./scripts/raspi/create_user.sh temp@thermal-camera:~/thermal-camera/
 rsync ./scripts/raspi/config.sh temp@thermal-camera:~/thermal-camera/
-ssh temp@thermal-camera 'cd thermal-camera && ./create_user.sh'
+#ssh temp@thermal-camera 'cd thermal-camera && ./create_user.sh'
