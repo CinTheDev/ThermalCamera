4b131575d357af7c98e831e1e60b80d1b2f45df4
Kevin Müller <mkevin2006@gmail.com>
2023-11-02 19:20:18 +0100

    Copy fbturbo configuration in script

--- scripts/raspi/scripts/config.sh
+++ scripts/raspi/scripts/config.sh
@@ -12,3 +12,4 @@ sudo apt install git xorg xserver-xorg-video-fbturbo lightdm eog
 sudo raspi-config nonint do_boot_behaviour B4
 
 # Configure fbturbo driver to use fb1
+sudo cp ./files/99-fbturbo.conf /usr/share/X11/xorg.conf.d/99-fbturbo.conf
--- scripts/raspi/scripts/display.sh
+++ scripts/raspi/scripts/display.sh
@@ -9,7 +9,7 @@ sudo git clone https://github.com/waveshare/LCD-show.git "$DRIVERS_PATH"
 cd $DRIVERS_PATH
 
 sudo chmod +x ./LCD35-show
-sudo ./LCD35-show lite
+sudo ./LCD35-show
 
 # TODO: Somehow make the drivers not restart
 #       and configure display further
