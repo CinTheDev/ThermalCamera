80813cc3e20400204655faed8e28a324c8986cdc
Kevin Müller <mkevin2006@gmail.com>
2023-11-02 21:41:25 +0100

    Add bit in sync.sh to copy thermal-camera binary extra

--- scripts/raspi/sync.sh
+++ scripts/raspi/sync.sh
@@ -2,5 +2,10 @@
 
 # Copy this directory onto raspi and connect via ssh
 
+cd thermal-camera
+cargo build
+cd ..
+
 rsync -r --delete ./scripts/raspi/ pi@thermal-camera:~/thermal-camera-config/
+rsync ./thermal-camera/target/armv7-unknown-linux-gnueabihf/debug/thermal-camera pi@thermal-camera:~/thermal-camera-config/files/
 ssh -t pi@thermal-camera 'cd thermal-camera-config; /bin/bash'
