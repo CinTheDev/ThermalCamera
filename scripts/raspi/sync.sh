8cec8f07bfe2a043229d8cd08b3a322458ffc565
Kevin Müller <mkevin2006@gmail.com>
2023-11-02 21:45:49 +0100

    Add progress to rsync

--- scripts/raspi/sync.sh
+++ scripts/raspi/sync.sh
@@ -6,6 +6,6 @@ cd thermal-camera
 cargo build
 cd ..
 
-rsync -r --delete ./scripts/raspi/ pi@thermal-camera:~/thermal-camera-config/
-rsync ./thermal-camera/target/armv7-unknown-linux-gnueabihf/debug/thermal-camera pi@thermal-camera:~/thermal-camera-config/files/
+rsync -P -r --delete ./scripts/raspi/ pi@thermal-camera:~/thermal-camera-config/
+rsync -P ./thermal-camera/target/armv7-unknown-linux-gnueabihf/debug/thermal-camera pi@thermal-camera:~/thermal-camera-config/files/
 ssh -t pi@thermal-camera 'cd thermal-camera-config; /bin/bash'
