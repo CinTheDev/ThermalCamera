cb42b0ff492435025768d58d514b3594662d2960
Kevin Müller <mkevin2006@gmail.com>
2023-11-02 20:01:39 +0100

    Add TODO for display

--- scripts/raspi/scripts/config.sh
+++ scripts/raspi/scripts/config.sh
@@ -3,13 +3,14 @@
 echo "Updating system..."
 
 sudo apt update
-sudo apt full-upgrade
+sudo -y apt full-upgrade
 
 echo "Installing X"
 
-sudo apt install git xorg xserver-xorg-video-fbturbo lightdm eog
+sudo -y apt install git xorg xserver-xorg-video-fbturbo lightdm ratpoison
 
 sudo raspi-config nonint do_boot_behaviour B4
 
 # Configure fbturbo driver to use fb1
-sudo cp ./files/99-fbturbo.conf /usr/share/X11/xorg.conf.d/99-fbturbo.conf
+# TODO: this should happen after display configuration
+#sudo cp ./files/99-fbturbo.conf /usr/share/X11/xorg.conf.d/99-fbturbo.conf
