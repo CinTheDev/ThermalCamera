71f57831529e1460bd224ab83a8483301e2895a6
Kevin Müller <mkevin2006@gmail.com>
2023-11-04 18:50:13 +0100

    Remove wrong X authorization and remove example picture

--- scripts/raspi/scripts/create_user.sh
+++ scripts/raspi/scripts/create_user.sh
@@ -8,7 +8,6 @@ if ! id "$USER_NAME" >/dev/null 2>&1; then
     sudo useradd $USER_NAME
     sudo passwd $USER_NAME
     sudo mkdir "/home/$USER_NAME"
-    sudo cp ~/.Xauthority "/home/$USER_NAME/.Xauthority"
 else
     echo "User \"$USER_NAME\" already exists"
 fi
