13e8f6f061e14c8fdaff0f46299ce70556f97be1
Kevin Müller <mkevin2006@gmail.com>
2023-11-04 19:51:41 +0100

    Remove solved TODOs

--- scripts/raspi/run.sh
+++ scripts/raspi/run.sh
@@ -8,16 +8,12 @@ if ! ping -q -c 1 -W 1 google.com >/dev/null; then
     exit 1
 fi
 
-# TODO: Implement all scripts
-
 # Make all scripts executable
 chmod -R +x ./scripts
 
 ./scripts/create_user.sh
 ./scripts/config.sh
 ./scripts/display.sh
-# [Copy important files to where they belong]
 ./scripts/enable_i2c.sh
-# [Systemctl]
 
 sudo reboot
