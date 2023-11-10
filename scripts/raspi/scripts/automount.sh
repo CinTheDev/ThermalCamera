18999054d257948c672661df5042b5e6810a91c2
Kevin Müller <mkevin2006@gmail.com>
2023-11-10 15:56:27 +0100

    Fix minor mistakes in new script

--- scripts/raspi/scripts/automount.sh
+++ scripts/raspi/scripts/automount.sh
@@ -2,9 +2,10 @@
 
 # Since installing usbmount from apt doesn't work for some reason, we'll have to compile it from source
 
-git clone https://github.com/rbrito/usbmount.git ~/usbmount
-cd ~/usbmount
+git clone https://github.com/rbrito/usbmount.git ~/usbmount/source
+cd ~/usbmount/source
 
 sudo apt update && sudo apt -y install debhelper build-essential
-sudo dpkg-buildpackage -us -uc b
+sudo dpkg-buildpackage -us -uc -b
+cd ..
 sudo dpkg -i usbmount_0.0.24_all.deb
