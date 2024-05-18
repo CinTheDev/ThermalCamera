872d7b298bf7770206b3727ca66188ad30069de3
Kevin Müller <mkevin2006@gmail.com>
2024-05-18 16:14:44 +0200

    Use driver repository from goodtft

--- scripts/raspi/scripts/display.sh
+++ scripts/raspi/scripts/display.sh
@@ -4,18 +4,19 @@
 
 DRIVERS_PATH='/opt/display-drivers'
 
-sudo git clone https://github.com/waveshare/LCD-show.git "$DRIVERS_PATH"
+sudo git clone https://github.com/goodtft/LCD-show.git "$DRIVERS_PATH"
+sudo chmod 755 -R "$DRIVERS_PATH"
 (
     cd $DRIVERS_PATH
 
     # To prevent the display drivers from restarting the whole system
     sudo chmod 0 /sbin/reboot
 
-    sudo chmod +x ./LCD35-show
+    #sudo chmod +x ./LCD35-show
     # For some reason, directly installing the lite variant leads to
     # touch drivers not being installed, so this has to be called twice.
     sudo ./LCD35-show
-    sudo ./LCD35-show lite
+    #sudo ./LCD35-show lite
 
     sudo chmod 0755 /sbin/reboot
 )
